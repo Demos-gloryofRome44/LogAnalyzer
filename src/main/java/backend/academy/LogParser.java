0255e1996c61d4a5f4dde0a695c88ec579da7d64
Demos <166336733+Demos-gloryofRome44@users.noreply.github.com>
2024-11-02 19:58:55 +0300

    Added the LogParser class with a static method  to parse individual log entries

---
+++ src/main/java/backend/academy/LogParser.java
@@ -0,0 +1,23 @@
+package backend.academy;
+
+import java.time.OffsetDateTime;
+import java.util.regex.Matcher;
+
+public class LogParser {
+
+    public static LogRecord parseLogLine(String line) {
+        Matcher matcher = LogRecord.LOG_PATTERN.matcher(line);
+        if (matcher.find()) {
+            String remoteAddr = matcher.group("remoteAddr");
+            OffsetDateTime timeLocal = OffsetDateTime.parse(matcher.group("timeLocal"), LogRecord.LOG_DATE_FORMATTER);
+            String request = matcher.group("request");
+            Integer status = Integer.parseInt(matcher.group("status"));
+            Long bodyBytesSent = Long.parseLong(matcher.group("bodyBytesSent"));
+            String referer = matcher.group("referer");
+            String userAgent = matcher.group("userAgent");
+
+            return new LogRecord(remoteAddr, timeLocal, request, status, bodyBytesSent, referer, userAgent);
+        }
+        return null;
+    }
+}
