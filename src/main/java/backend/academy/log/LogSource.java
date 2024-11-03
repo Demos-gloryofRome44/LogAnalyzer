c1c223b67d0bf015abdacc696ddba41f602d68b0
Demos <166336733+Demos-gloryofRome44@users.noreply.github.com>
2024-11-03 23:04:08 +0300

    Fix getters method

--- src/main/java/backend/academy/log/LogSource.java
+++ src/main/java/backend/academy/log/LogSource.java
@@ -1,7 +1,9 @@
 package backend.academy.log;
 
+import lombok.Getter;
 import java.nio.file.Paths;
 
+@Getter
 public class LogSource {
     private final String path;
     private final LogType type;
@@ -14,14 +16,6 @@ public class LogSource {
     public LogSource(String path, LogType type) {
         this.path = path;
         this.type = type;
-    }
-
-    public String getPath() {
-        return path;
-    }
-
-    public LogType getType() {
-        return type;
     }
 
     @Override
